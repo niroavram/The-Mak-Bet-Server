511c15a0d1ca298feebc9110ec9c7490021ba4e5
niroavram <69424993+niroavram@users.noreply.github.com>
2021-09-06 01:23:17 +0300

    ok

--- app.js
+++ app.js
@@ -49,9 +49,9 @@ app.use(authEvent,authLeague,authUserbets);
   // setInterval(function() {
   //   finishGames();
   //   }, 1000*15,);
-  let S="id,name,age\n1,Jack,68"
-  let C="age"
-  Testing(S,C)
+  // let S="id,name,age\n1,Jack,68"
+  // let C="age"
+  // Testing(S,C)
 
 app.listen(PORT,()=>{
     console.log("server running on PORT",PORT)
